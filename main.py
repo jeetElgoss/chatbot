c675f02072a0f89ab70369da29c38f8fc644e178
jeetElgoss <prasannajeet@elgoss.com>
2024-02-26 15:02:47 +0530

    addded requirment.txt

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from open_ai_chatbot import ChatBot
 
 
-# obj_chatBot = ChatBot()
+obj_chatBot = ChatBot()
 # obj_chatBot.call_chat_with_openai()
 
 
-def character_count():
-    char_count = {}
-    data = "hello sonu"
-    for x in data:
-        if x in char_count:
-            char_count[x] += 1
-        else:
-            char_count[x] = 1
-    return char_count
-
-
-a = character_count()
-print(a)
+# def character_count():
+#     char_count = {}
+#     data = "hello sonu"
+#     for x in data:
+#         if x in char_count:
+#             char_count[x] += 1
+#         else:
+#             char_count[x] = 1
+#     return char_count
+#
+#
+# a = character_count()
+# print(a)
